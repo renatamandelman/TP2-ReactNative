e15ef338db8b4f2ec437703cf5a4fcbef6df8722
Android Studio <devandroid@ort>
2022-10-31 10:43:31 -0300

    imagenFondo

--- src/Components/imagenFondo.js
+++ src/Components/imagenFondo.js
@@ -1,17 +1,15 @@
 import React, { useState, useEffect }  from 'react';
-import { Text,View, StyleSheet, Button,Image,ImageBackground } from 'react-native';
+import { TextInput,View, StyleSheet, Button,Image,ImageBackground,Dimensions } from 'react-native';
 // import { Video, AVPlaybackStatus } from 'expo-av';
 // import logo from './assets/logo.png'; 
 import * as ImagePicker from 'expo-image-picker';
 
+const screenHeight = Dimensions.get('window').height;
+ const screenWidth = Dimensions.get('window').width;
+const imagenFondo = () => {
 
 
-export default function phoneNumber(){
-    // const video = React.useRef(null);
-    // const video1 = React.useRef(null);
-    // const [status, setStatus] = React.useState({});
-    // const [status1, setStatus1] = React.useState({});
-    const [image, setImage] = useState(null);
+const [image, setImage] = useState(null);
 
     const pickImage = async () => {
       // No permissions request is necessary for launching the image library
@@ -28,59 +26,98 @@ export default function phoneNumber(){
         setImage(result.uri);
       }
     };
+  return (
+    <View>
+    <Button title="Pick Image" onPress={pickImage()}/>   
+      <ImageBackground source={{uri:{image} }}
+        resizeMode="stretch"
+        style={styles.img}> 
+    
+      </ImageBackground>
+    </View>
+  );
+};
+  
+export default imagenFondo;
+  
+const styles = StyleSheet.create({
+  img: {
+    height: screenHeight,
+    width: screenWidth,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 2,
+    padding: 10,
+  },
+});
+// export default function imagenFondo(){
+//     // const video = React.useRef(null);
+//     // const video1 = React.useRef(null);
+//     // const [status, setStatus] = React.useState({});
+//     // const [status1, setStatus1] = React.useState({});
+//     const [image, setImage] = useState(null);
 
-    return (
+//     const pickImage = async () => {
+//       // No permissions request is necessary for launching the image library
+//       let result = await ImagePicker.launchImageLibraryAsync({
+//         mediaTypes: ImagePicker.MediaTypeOptions.All,
+//         allowsEditing: true,
+//         aspect: [4, 3],
+//         quality: 1,
+//       });
+  
+//       console.log(result);
+  
+//       if (!result.cancelled) {
+//         setImage(result.uri);
+//       }
+//     };
+
+//     return (
       
-    <View style={styles.container}>
-        {/* <Image source={logo}style={styles.logo} /> */}
-        <Text style={styles.titleText}> Public Video  </Text>
-        <Button title="Pick Image" onPress={pickImage}/>
-        <ImageBackground 
-        
-    style={styles.img}
-    source={{uri: image}}/>
+//     <View style={styles.container}>
+//         <Button title="Pick Image" onPress={pickImage}/>
+//         <ImageBackground style={styles.img} source={{uri: image}}         resizeMode="stretch"
+// />
     
 
-       </View>
-    );
-  }
+//        </View>
+//     );
+//   }
   
 
   
-  const styles = StyleSheet.create({
-    container: {
-     flex: 1,
-      justifyContent: 'center',
-      backgroundColor: '#ecf0f1',
-      alignItems:'center'
-    },
+//   const styles = StyleSheet.create({
+//     container: {
+//      flex: 1,
+//       justifyContent: 'center',
+//       backgroundColor: '#ecf0f1',
+//       alignItems:'center'
+//     },
 
-    buttons: {
-      flexDirection: 'row',
-      justifyContent: 'center',
-      alignItems: 'center',
-    },
-    titleText: {
+//     buttons: {
+//       flexDirection: 'row',
+//       justifyContent: 'center',
+//       alignItems: 'center',
+//     },
+//     titleText: {
       
-      fontSize: 20,
-      fontWeight: 'bold',
-      textAlign: 'center',
-      marginVertical: 25,
+//       fontSize: 20,
+//       fontWeight: 'bold',
+//       textAlign: 'center',
+//       marginVertical: 25,
   
-    },
-    img: {
-      height: screenHeight,
-      width: screenWidth,
-      justifyContent: 'center',
-      alignItems: 'center',
-    },
-    // logo:{ 
-    //   width: 200,
-    //   height: 54,
-     
-    //  },
-     Perfil:{
-
-     }
+//     },
+//     img: {
+//       height: screenHeight,
+//       width: screenWidth,
+//       justifyContent: 'center',
+//       alignItems: 'center',
+//     },
+   
     
-  });
+//   });
